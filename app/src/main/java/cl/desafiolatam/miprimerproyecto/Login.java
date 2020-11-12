01610afc0dabda3329ca10d986b7f9c50d386c40
Patricio García A <pgarcia@serlitoral.cl>
2020-11-12 15:17:11 -0300

    Modificado

--- app/src/main/java/cl/desafiolatam/miprimerproyecto/Login.java
+++ app/src/main/java/cl/desafiolatam/miprimerproyecto/Login.java
@@ -2,13 +2,17 @@ package cl.desafiolatam.miprimerproyecto;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.widget.Button;
 
 public class Login extends AppCompatActivity {
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
     }
 }
--- app/src/main/java/cl/desafiolatam/miprimerproyecto/RegistroUsuario.java
+++ app/src/main/java/cl/desafiolatam/miprimerproyecto/RegistroUsuario.java
@@ -10,5 +10,6 @@ public class RegistroUsuario extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_registro_usuario);
+
     }
 }
